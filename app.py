7998bd5c2d0e44aa85f380bbf6aad441daa46d25
mohdnishal <nishalutd@gmail.com>
2025-03-26 05:35:47 +0530

    changed the ml model to clustering model updated flask

--- app.py
+++ app.py
@@ -185,5 +185,6 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=10000)
